ae3652f17f6b3b3f9e7ce0aa828f0dbb7fe8137e
Ben Gebre-Medhin <BeniamG@users.noreply.github.com>
2017-05-02 15:32:16 -0700

    final version

--- run_ipynb_test.sh
+++ run_ipynb_test.sh
@@ -4,10 +4,20 @@
 
 if [ ! -d ../data/scratch_dir ]; then
   mkdir -p ../data/scratch_dir;
+else
+	echo "Directory already exist.  Check contents before continuing"
 fi
 
 notebook=$1
 scriptname="$(basename $notebook .ipynb)".py
 
+#this uses nbconvert, a Jupyter library, to create a .py and run it in python3
 jupyter nbconvert --to script --execute ${notebook} && python3 ${scriptname}
+
+#this moves the .py to the data archive folder for future reproduceability
 mv ${scriptname} ../data/scratch_dir
+
+#This creates the variable to be passed to the new file name
+date=$`date +%Y-%m-%d_at_%H-%M`
+#This renames the folder for future reference
+mv ../data/scratch_dir ../data/$date
