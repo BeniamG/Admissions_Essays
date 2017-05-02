883b35cb94ad9b8adda926a08cfff83138d673dc
Ben Gebre-Medhin <BeniamG@users.noreply.github.com>
2017-05-02 14:45:16 -0700

    testing new way to save script archive

---
+++ run_ipynb_test.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# First argument is the notebook you would like to run
+# when in directory with the ipython notbook run './run_ipynb.sh Admissions_Essays_Production.ipynb'
+
+if [ ! -d ../data/scratch_dir ]; then
+  mkdir -p ../data/scratch_dir;
+fi
+
+notebook=$1
+scriptname="$(basename $notebook .ipynb)".py
+
+jupyter nbconvert --to script --execute ${notebook} && python3 ${scriptname}
+rm ${scriptname}
