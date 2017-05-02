b24736f8f81279161d204f2c57e900d517560e5c
Ben Gebre-Medhin <BeniamG@users.noreply.github.com>
2017-05-02 16:37:15 -0700

    prepared for test on mercury

--- run_ipynb.sh
+++ run_ipynb.sh
@@ -1,8 +1,26 @@
 #!/usr/bin/env sh
 # First argument is the notebook you would like to run
+# when in directory with the ipython notbook run './run_ipynb.sh Admissions_Essays_Production.ipynb'
+
+if [ ! -d ../data/scratch_dir ]; then
+  mkdir -p ../data/scratch_dir;
+else
+	echo "Directory already exist.  Check contents before continuing"
+fi
 
 notebook=$1
 scriptname="$(basename $notebook .ipynb)".py
 
+#This creates the variable to be passed to the new file name
+date=$(date +%Y-%m-%d_at_%H-%M)
+
+date_name=$date'-'$scriptname
+
+#this uses nbconvert, a Jupyter library, to create a .py and run it in python3
 jupyter nbconvert --to script --execute ${notebook} && python3 ${scriptname}
-rm ${scriptname}
+
+#this moves the .py to the data archive folder for future reproduceability
+mv ${scriptname} ../data/scratch_dir/$date_name
+
+#This renames the folder for future reference
+mv ../data/scratch_dir ../data/$date
