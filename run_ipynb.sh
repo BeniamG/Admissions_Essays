c8cf56cc906f2246a98ae66e3a586da03ea91b0a
Ben Gebre-Medhin <BeniamG@users.noreply.github.com>
2017-05-02 17:13:19 -0700

    fixed file name format

--- run_ipynb.sh
+++ run_ipynb.sh
@@ -2,19 +2,22 @@
 # First argument is the notebook you would like to run
 # when in directory with the ipython notbook run './run_ipynb.sh Admissions_Essays_Production.ipynb'
 
+# This makes a new folder called scratch_dir where we put our output
 if [ ! -d ../data/scratch_dir ]; then
   mkdir -p ../data/scratch_dir;
 else
 	echo "Directory already exist.  Check contents before continuing"
 fi
 
-notebook=$1
-scriptname="$(basename $notebook .ipynb)".py
-
-#This creates the variable to be passed to the new file name
+# This creates a date variable
 date=$(date +%Y-%m-%d_at_%H-%M)
 
-date_name=$scriptname'-'$date
+notebook=$1
+scriptname="$(basename $notebook .ipynb)".py
+date_name="$(basename $notebook .ipynb)"'-'$date.py
+
+
+#date_name=$scriptname'-'$date
 
 #this uses nbconvert, a Jupyter library, to create a .py and run it in python3
 jupyter nbconvert --to script --execute ${notebook} && python3 ${scriptname}
